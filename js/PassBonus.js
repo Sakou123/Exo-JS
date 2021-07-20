3932bc32e57092932c68f0c37ba8e50d67d74d06
Sakou <vigneronlucweb@gmail.com>
2021-07-20 12:05:14 +0200

    Min 8 chars

--- js/PassBonus.js
+++ js/PassBonus.js
@@ -1,6 +1,6 @@
 // GENERATEUR DE MOT DE PASSE! BONUS
 
-const character = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const character = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "&", "é", "(", "-", "è", "_", "ç", "à", ")", "=", "~", "#", "{", "[", "|", "`", "@", "]", "}", "^", "$", "ù", "*", ",", ";", ":", "!", "¨", "£", "%", "µ", "?", ".", "/", "§"];
 const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const special = ["&", "é", "(", "-", "è", "_", "ç", "à", ")", "=", "~", "#", "{", "[", "|", "`", "@", "]", "}", "^", "$", "ù", "*", ",", ";", ":", "!", "¨", "£", "%", "µ", "?", ".", "/", "§"];
 const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -29,6 +29,10 @@ function shuffeledWord(word) {
     }
     return shuffledWord;
 }
-
-password(8)                        //J'appel ma fonction pour obtenir une suite de caractère
-console.log(shuffeledWord(result))  //J'affiche mon mdp qui s'est fait shuffle pour ne pas avoir la même chose
+let security = 8
+if (security>=8){
+    password(security)                          //J'appel ma fonction pour obtenir une suite de caractère
+    console.log(shuffeledWord(result))          //J'affiche mon mdp qui s'est fait shuffle pour ne pas avoir la même chose
+}else{
+    console.log("Min 8 caractère.")
+}
